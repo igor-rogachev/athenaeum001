92594054b4022a12254ada09b2505d275447818f
irogachev <igorsjob@gmail.com>
2019-04-05 15:06:34 +0600

    Теперь корректно работает notNull констрен на genre. Этот констрейн работает только при создании таблицы. Если таблица уже есть, то чтобы констрейн применился, нужно убить таблицу и при следующем запуске она будет создана уже с этим констрейном.

---
+++ src/main/java/com/igorrogachev/athenaeum/domain/Book.java
@@ -0,0 +1,51 @@
+package com.igorrogachev.athenaeum.domain;
+
+        import javax.persistence.*;
+        import javax.validation.constraints.NotEmpty;
+        import javax.validation.constraints.NotNull;
+        import java.util.Date;
+
+@Entity
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"title", "year"})})
+public class Book {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    @NotNull(message = "Title may not be null")
+    @NotEmpty(message = "Title may not be empty")
+    private String title;
+
+    @NotNull(message = "Year may not be null")
+    private Date year;
+
+    public Book() {
+    }
+
+    public Book(@NotNull(message = "Title may not be null")
+                @NotEmpty(message = "Title may not be empty") String title,
+                @NotNull(message = "Year may not be null") Date year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
--- src/main/java/com/igorrogachev/athenaeum/domain/Genre.java
+++ src/main/java/com/igorrogachev/athenaeum/domain/Genre.java
@@ -1,9 +1,10 @@
 package com.igorrogachev.athenaeum.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
-// @Table
 @Table(uniqueConstraints={@UniqueConstraint(columnNames={"name"})})
 public class Genre {
 
@@ -11,23 +12,33 @@ public class Genre {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
 
-    // @Column(unique = true)
+    /*
     @Column(nullable=false)
+    это как бы работает (кривовато: пустое поле в строку все же пишется, но ошибка так же стартует :-) ),
+    но почему-то в DDL для поля NorNull не подоставляет
+    Отсюда
+    https://www.baeldung.com/java-bean-validation-not-null-empty-blank
+    берем конструкцию
+    @NotNull(message = "Name may not be null")
+    эта конструкция ведет себя аналогичным образом
+    пока оставим это
+    Похоже, что эта конструкция работает только при создании таблицы
+     */
+    // @Column(nullable=false)
+    @NotNull(message = "Name may not be null")
+    @NotEmpty(message = "Name may not be empty")
     private String name;
 
     public Genre() {
     }
 
-    public Genre(String name) {
+    public Genre(@NotNull(message = "Name may not be null")
+                 @NotEmpty(message = "Name may not be empty") String name) {
         this.name = name;
     }
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getName() {
